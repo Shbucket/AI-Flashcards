4db9b70cd018845555a8e0bbb673e411daa065c1
shomari <shomarijoseph@gmail.com>
2024-08-14 17:17:44 -0400

    Fixed errors and recreated project

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvYfh4FMLIqU9BRdfRih4FlrFlnkktSok",
+  authDomain: "ai-flashcards-15574.firebaseapp.com",
+  projectId: "ai-flashcards-15574",
+  storageBucket: "ai-flashcards-15574.appspot.com",
+  messagingSenderId: "50430365827",
+  appId: "1:50430365827:web:9847400d2794f2879b4bb6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
