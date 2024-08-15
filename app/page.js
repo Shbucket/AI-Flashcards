aa32a2e382edba801c546f91c81e076e887c9529
shomari <shomarijoseph@gmail.com>
2024-08-15 16:46:16 -0400

    added what was needed to run with clerk

--- app/page.js
+++ app/page.js
@@ -1,7 +1,46 @@
+"use client"
+
+import {
+  AppBar,
+  Container,
+  Typography,
+  Toolbar,
+  Grid,
+  Button,
+} from "@mui/material";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Head from "next/head";
+import { ClerkProvider } from "@clerk/clerk-react";
+
 
 export default function Home() {
   return (
-    <>
-    </>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <Container maxWidth="100vw">
+        <Head>
+          <title>Flashcard Saas</title>
+          <meta name="description" content="Create flashcard from your test" />
+        </Head>
+
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+              Flashcard SaaS
+            </Typography>
+            <SignedOut>
+              <Button color="inherit" href="/sign-in">
+                Login
+              </Button>
+              <Button color="inherit" href="/sign-up">
+                Sign Up
+              </Button>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </ClerkProvider>
   );
 }
